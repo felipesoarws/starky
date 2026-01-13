50196808363ddb621ee0cab4faa26c50fc8472ee
felipesoarws <felipesoarwz@gmail.com>
2026-01-13 17:05:46 -0300

    fix: new font added

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -16,7 +16,7 @@ export const Button: React.FC<ButtonProps> = ({
   ...props
 }) => {
   const baseStyles =
-    "inline-flex items-center justify-center font-medium transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-black disabled:opacity-50 disabled:cursor-not-allowed rounded-full";
+    "cursor-pointer inline-flex items-center justify-center font-medium transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-black disabled:opacity-50 disabled:cursor-not-allowed rounded-full";
 
   const variants = {
     primary:
